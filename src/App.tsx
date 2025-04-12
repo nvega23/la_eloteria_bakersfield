b654f7d6755e451996031cfd271fb8a6d4b62aea
Nestor Vega <vega.nestor1@gmail.com>
2025-04-12 11:10:00 -0700

    github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import logo from '../public/largeLogo.png';
+import logo from '/largeLogo.png'; // Use / if it's in public folder
+
+const repoName = 'la_eloteria_bakersfield';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename={`/${repoName}`}>
       <div className="flex flex-col min-h-screen">
         <nav className="bg-red-500 text-black p-4 flex items-center justify-between">
           <div className="flex items-center">
@@ -15,8 +17,8 @@ function App() {
           </div>
           <div className="space-x-4">
             <Link to="/" className="hover:underline">Menú</Link>
-            <Link to="/about" className="hover:underline">Sobre Nosotros</Link>
-            <Link to="/contact" className="hover:underline">Contáctanos</Link>
+            <Link to="/Sobre_Nosotros" className="hover:underline">Sobre Nosotros</Link>
+            <Link to="/Contáctanos" className="hover:underline">Contáctanos</Link>
           </div>
         </nav>
 
@@ -24,8 +26,8 @@ function App() {
           <div className="w-full max-w-4xl px-4 py-8">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
+              <Route path="/Sobre_Nosotros" element={<About />} />
+              <Route path="/Contáctanos" element={<Contact />} />
             </Routes>
           </div>
         </main>
@@ -34,7 +36,7 @@ function App() {
           &copy; {new Date().getFullYear()} La Eloteria
         </footer>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
