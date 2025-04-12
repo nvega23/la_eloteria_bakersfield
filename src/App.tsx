d2b89cc3134e228513e64ae2d8aab2fb0c0c41ca
Nestor Vega <vega.nestor1@gmail.com>
2025-04-12 10:34:16 -0700

    updated import issu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import logo from '../public/largeLogo.png';
